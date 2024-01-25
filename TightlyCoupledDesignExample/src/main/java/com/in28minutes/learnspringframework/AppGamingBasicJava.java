cde103cb386ac4c09c84f008101d378500e653fe
Karthik Singh <karthiksheru97@gmail.com>
2024-01-24 20:14:58 -0500

    Tight Coupling DesignApproach using Java (GameRunner class Calling different games like Mario,SuperContra).

---
+++ TightlyCoupledDesignExample/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -0,0 +1,17 @@
+package com.in28minutes.learnspringframework;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.MarioGame;
+import com.in28minutes.learnspringframework.game.SuperContraGame;
+
+public class AppGamingBasicJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //MarioGame  marioGame  = new MarioGame();
+        SuperContraGame superContraGame = new SuperContraGame();
+		GameRunner gameRunner = new GameRunner(superContraGame);
+        gameRunner.run();
+	}
+
+}
